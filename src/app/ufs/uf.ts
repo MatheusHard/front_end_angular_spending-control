8ee9542fc58f26cfa6d23a626462186afbdd6de8
MatheusHard <matheushard2013@gmail.com>
2021-05-24 20:43:42 -0300

    first commit

---
+++ src/app/ufs/uf.ts
@@ -0,0 +1,4 @@
+export class Uf {
+id: number;
+descricao_uf: string;
+}
---
+++ src/app/ufs/ufs.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ufs',
+  templateUrl: './ufs.component.html',
+})
+export class UfsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
